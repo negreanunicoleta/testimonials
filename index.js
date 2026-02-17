361aa94cde6c69c0110e65a0879d1e6fa7c3a8d0
Nico N. <86585461+negreanunicoleta@users.noreply.github.com>
2026-02-17 17:11:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const testimonial = [
     name: "Emma Thompson",
     photoUrl:
       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    text: "I am extremely satisfied with the innovative mobile phones offered by apple. The sleek design and advanced features surpassed my expectations. I highly recommend apple to anyone looking for top-notch technology.",
+    text: "I am beyond impressed with Apple's innovative mobile phones. The sleek design and cutting-edge technology make them stand out from the rest. I highly recommend Apple to anyone looking for top-quality devices.",
   },
   {
     name: "Sophie Anderson",
     photoUrl:
       "https://images.unsplash.com/photo-1546961329-78bef0414d7c?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    text: "I am extremely satisfied with the exceptional quality and innovation of mobile phones from apple. Their products are sleek, reliable, and user-friendly. I highly recommend apple to anyone in search of top-notch mobile devices.",
+    text: "I am extremely impressed with apple's innovative mobile phones. The sleek design, advanced features, and user-friendly interface make apple stand out from the competition. I highly recommend apple for anyone looking for a top-notch mobile device.",
   },
 ];
 const imgEl = document.querySelector("img");
@@ -38,4 +38,5 @@ function updateTestimonial() {
     updateTestimonial();
   }, 1000);
 }
+
 
